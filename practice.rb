0373b09e99f91ff7936a831c1ad1438628082a1b
asifaly <asifaly82@yahoo.com>
2016-04-08 19:33:13 +0530

    interpolation, function calls, closure

--- practice.rb
+++ practice.rb
@@ -1,7 +1,18 @@
-def factorial(n)
-  if n == 1
-    return 1
-  else
-    return n*factorial(n-1)
-  end
+def hello
+  puts 'hello world'
 end
+
+def hello2(n)
+  puts "hello #{n}"
+end
+
+def multiply(table, upto)
+  (1..upto).each { |e| puts "#{table} x #{e} = #{table * e}"}
+end
+
+# function can be called with or without paranthesis
+hello
+hello2 'world'
+hello2('world')
+multiply 5,2
+multiply(5,2)
